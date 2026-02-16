31c6bdc7498a28bcf1c2269ad4fc8ae400f7516d
yaoshikuro <yukivoron@gmail.com>
2026-02-16 22:32:23 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Салман Аббасов");
+Console.WriteLine(DateTime.Now);
